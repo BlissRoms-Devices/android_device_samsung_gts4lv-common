19a804655c0b8f0f96afe38cb11789cc30b30eb6
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2025-04-23 15:33:55 -0500

    gts4lv-common: Enable freeform multi-windows

--- gts4lv.mk
+++ gts4lv.mk
@@ -153,6 +153,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.samsung
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
